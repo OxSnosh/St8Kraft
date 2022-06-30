0106b989fab04c780224e8e083da4292b05b36a2
OxSnosh <OxSnosh@proton.me>
2022-06-29 20:55:29 -0500

    ready

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -5,10 +5,10 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // contract for buying commodities (land, infrastructure, tech)
 contract CommodityMarketplace {
-    function buyTank(uint256 amountIn) {
-        IERC20(_tokenAddress).approve(msg.sender, amountIn);
-        IERC20(_tokenAddress).transferFrom(msg.sender, address(this), amountIn);
-    }
+    // function buyTank(uint256 amountIn) {
+    //     IERC20(_tokenAddress).approve(msg.sender, amountIn);
+    //     IERC20(_tokenAddress).transferFrom(msg.sender, address(this), amountIn);
+    // }
 }
 
 // contract for buing military (soldiers, tanks, planes, nukes)
