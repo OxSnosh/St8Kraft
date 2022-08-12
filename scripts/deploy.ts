b2cf1d72017a803c2e9c8b5d5e489795c2779f02
OxSnosh <OxSnosh@proton.me>
2022-08-11 21:03:38 -0500

    updated deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -16,21 +16,26 @@ async function main() {
     const metanationsgvtoken = await MetaNatonsGovToken.deploy(INITIAL_SUPPLY)
     await metanationsgvtoken.deployed()
     console.log(`MetaNationsGovToken deployed to ${metanationsgvtoken.address}`)
+
+    const CountryParameters = await hre.ethers.getContractFactory("CountryParametersContract")
+    const countryparameter = await CountryParameters.deploy()
+    await countryparameter.deployed()
+    console.log(`CountryParameters deployed to ${countryparameter.address}`)  
     
     const CountryMinter = await hre.ethers.getContractFactory("CountryMinter")
-    const countryminter = await CountryMinter.deploy(warbucks.address)
+    const countryminter = await CountryMinter.deploy(warbucks.address, countryparameter.address)
     await countryminter.deployed()
     console.log(`CountryMinter deployed to ${countryminter.address}`)
 
-    const Marketplace = await hre.ethers.getContractFactory(
-        "CommodityMarketplace"
-    )
-    const marketplace = await Marketplace.deploy(
-        warbucks.address,
-        countryminter.address
-    )
-    await marketplace.deployed()
-    await console.log(`Marketplace deployed to ${marketplace.address}`)
+    // const Marketplace = await hre.ethers.getContractFactory(
+    //     "CommodityMarketplace"
+    // )
+    // const marketplace = await Marketplace.deploy(
+    //     warbucks.address,
+    //     countryminter.address
+    // )
+    // await marketplace.deployed()
+    // await console.log(`Marketplace deployed to ${marketplace.address}`)
 }
 
 main().catch((error) => {
