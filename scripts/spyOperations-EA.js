0aa32f38589e87c15659022537ebbfcf056f6e63
OxSnosh <OxSnosh@proton.me>
2023-05-21 17:34:15 -0500

    debugging getting spy operations script to run on localhost

--- scripts/spyOperations-EA.js
+++ scripts/spyOperations-EA.js
@@ -1,26 +1,31 @@
-import {ethers} from "hardhat";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import * as metadata from "../script-metadata.json";
-import { Signer } from "ethers";
-import { TreasuryContract } from '../typechain-types/contracts/Treasury.sol/TreasuryContract';
-import { TechnologyMarketContract } from '../typechain-types/contracts/TechnologyMarket.sol/TechnologyMarketContract';
-import { ForcesContract } from '../typechain-types/contracts/Forces.sol/ForcesContract';
-import { MilitaryContract } from '../typechain-types/contracts/Military.sol/MilitaryContract';
-import { WarContract } from '../typechain-types/contracts/War.sol/WarContract';
-import { CountryMinter } from '../typechain-types/contracts/CountryMinter';
-import { InfrastructureMarketContract } from "../typechain-types";
+const {ethers} = require("hardhat");
+// import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+const metadata = require("../script-metadata.json");
+// import { Signer } from "ethers";
+// import { TreasuryContract } from '../typechain-types/contracts/Treasury.sol/TreasuryContract';
+// import { TechnologyMarketContract } from '../typechain-types/contracts/TechnologyMarket.sol/TechnologyMarketContract';
+// import { ForcesContract } from '../typechain-types/contracts/Forces.sol/ForcesContract';
+// import { MilitaryContract } from '../typechain-types/contracts/Military.sol/MilitaryContract';
+// import { WarContract } from '../typechain-types/contracts/War.sol/WarContract';
+// import { CountryMinter } from '../typechain-types/contracts/CountryMinter';
+// import { InfrastructureMarketContract } from "../typechain-types";
+
+const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545")
 
 const initiateSpyOperationTest = async () => {
 
-    let signers = await ethers.getSigners();
-    let signer0 = await ethers.getSigner("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266") as any
-    let signer1 = await ethers.getSigner("0x70997970C51812dc3A010C7d01b50e0d17dc79C8") as any
-    let signer2 = await ethers.getSigner("0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC") as any
+    const signer0 = provider.getSigner(0)
+    const signer1 = provider.getSigner(1)
+    const signer2 = provider.getSigner(2)
+
+    console.log("here?")
 
     const countryMinterAddress = metadata.HARDHAT.countryminter.address;
     const countryMinterAbi = metadata.HARDHAT.countryminter.ABI;
     const countryminter = new ethers.Contract(countryMinterAddress, countryMinterAbi, signer0);
   
+    console.log("here? 2")
+
     const forcesContractAddress = metadata.HARDHAT.forcescontract.address;
     const forcesContractAbi = metadata.HARDHAT.forcescontract.ABI;
     const forcescontract = new ethers.Contract(forcesContractAddress, forcesContractAbi, signer0);
@@ -31,19 +36,19 @@ const initiateSpyOperationTest = async () => {
 
     const TreasuryContractAddress = metadata.HARDHAT.treasurycontract.address;
     const TreasuryContractAbi = metadata.HARDHAT.treasurycontract.ABI;
-    const treasurycontract = new ethers.Contract(TreasuryContractAddress, TreasuryContractAbi, signer0) as TreasuryContract;
+    const treasurycontract = new ethers.Contract(TreasuryContractAddress, TreasuryContractAbi, signer0)
 
     const InfrastructureContractAddress = metadata.HARDHAT.infrastructurecontract.address;
     const InfrastructureContractAbi = metadata.HARDHAT.infrastructurecontract.ABI;
-    const infrastructuremarketplace = new ethers.Contract(InfrastructureContractAddress, InfrastructureContractAbi, signer0) as InfrastructureMarketContract;
+    const infrastructuremarketplace = new ethers.Contract(InfrastructureContractAddress, InfrastructureContractAbi, signer0)
 
     const TechnologyMarketContractAddress = metadata.HARDHAT.technologymarketcontract.address;
     const TechnologyMarketContractAbi = metadata.HARDHAT.technologymarketcontract.ABI;
-    const technologymarketcontrat = new ethers.Contract(TechnologyMarketContractAddress, TechnologyMarketContractAbi, signer0) as TechnologyMarketContract;
+    const technologymarketcontrat = new ethers.Contract(TechnologyMarketContractAddress, TechnologyMarketContractAbi, signer0)
 
     const MilitaryContractAddress = metadata.HARDHAT.militarycontract.address;
     const MilitaryContractAbi = metadata.HARDHAT.militarycontract.ABI;
-    const militarycontract = new ethers.Contract(MilitaryContractAddress, MilitaryContractAbi, signer0) as MilitaryContract;
+    const militarycontract = new ethers.Contract(MilitaryContractAddress, MilitaryContractAbi, signer0)
 
     await countryminter.connect(signer1).generateCountry(
         "TestRuler",
